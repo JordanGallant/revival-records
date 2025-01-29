94b8432c0ec255ff94bd23444e029ac56550285e
JordanGallant <39-12602@sae.edu.za>
2025-01-29 09:54:49 +0200

    fixing hydra types

--- src/types/hydra-synth.d.ts
+++ src/types/hydra-synth.d.ts
@@ -3,6 +3,8 @@ declare module "hydra-synth" {
     export const osc: any;
     export const noise: any;
     export const time: any;
+    export const s0: any;
+    export const src: any;
   
     export default Hydra;
   }
