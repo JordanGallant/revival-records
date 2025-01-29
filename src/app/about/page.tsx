c684187a26b5db097b17b864f2ac2b23b3c207c4
JordanGallant <39-12602@sae.edu.za>
2025-01-29 13:52:56 +0200

    adding about me div

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,6 +13,12 @@ const Listen: React.FC = () => {
         <NavBar />
         
       </div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-start z-30 bg-stone-200 bg-opacity-10 w-[90%] h-2/3 group hover:bg-opacity-50 rounded-lg">
+  <h1 className="font-badeen text-5xl text-left mt-4">About Us</h1>
+</div>
+
+
+
 
       <AudioVisualizer/>
 
