087419ddf573092db644be5a53386490312b63ee
JordanGallant <39-12602@sae.edu.za>
2025-01-29 09:41:27 +0200

    merge

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,17 +2,13 @@
 import React from 'react';
 
 import NavBar from '../_components/navbar';
-<<<<<<< HEAD
 import HydraCanvas from "../shaders/Dance";
-=======
-import HydraCanvas from "../shaders/dance";
->>>>>>> 063aa60d1f3afcfd0b194e1a6b73b55a8950d1ed
 const Listen: React.FC = () => {
     return (
       <>
         <div className="absolute top-0 left-0 w-full z-20">
           <NavBar />
-          Hello
+          
         </div>
         <div className="relative z-0 min-h-[100vh]">
                 
