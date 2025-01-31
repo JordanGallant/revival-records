3c85fd2487ab5c21f870f8ec917c7430d701759c
JordanGallant <39-12602@sae.edu.za>
2025-01-31 08:41:02 +0200

    music

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,6 +15,7 @@ const Listen: React.FC = () => {
       </div>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-start z-30 bg-stone-200 bg-opacity-10 w-[90%] h-2/3 group hover:bg-opacity-50 rounded-lg">
   <h1 className="font-badeen text-5xl text-left mt-4">About Us</h1>
+  <p> we are a UKG record label based in the Netherlands</p>
 </div>
 
 
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -20,8 +20,8 @@ const Music: React.FC = () => {
         <Video />
         
         
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center z-30 bg-stone-200 bg-opacity-10 w-[90%]  h-2/3 group hover:bg-opacity-50 rounded-lg">
-          <h1 className="font-badeen text-5xl bg-opacity-75"> The Revival Playlist</h1>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-start z-30 bg-stone-200 bg-opacity-10 w-[90%] h-2/3 group hover:bg-opacity-50 rounded-lg">
+      <h1 className="font-badeen text-5xl text-left mt-4">The Revival Playlist</h1>
           
         </div>
       </div>
