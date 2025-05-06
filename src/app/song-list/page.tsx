5ae564461f8d330bf45f94c6be750aa73f16b223
Jordan <39-12602@sae.edu.za>
2025-05-06 09:20:06 +0200

    song-list

--- src/app/song-list/page.tsx
+++ src/app/song-list/page.tsx
@@ -60,7 +60,7 @@ const Reload: React.FC = () => {
         <>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-start z-30 bg-stone-200 bg-opacity-10 w-[90%] h-2/3 group hover:bg-opacity-50 rounded-lg">
                 <h1 className="font-badeen text-5xl text-left mt-4 text-black">
-                    Download Songs
+                    Download All Songs
                 </h1>
                 <div className="w-full p-4">
 
