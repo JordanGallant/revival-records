ed6ab07ba0104f4fc811676bfd5122db055c4eb6
Jordan <39-12602@sae.edu.za>
2025-05-14 10:59:48 +0200

    re-add nav in blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from "next/link";
 import dynamic from 'next/dynamic';
-
+import Navigator from '../_components/navigator';
 
 const HydraCanvas = dynamic(() => import("../shaders/Hydra"), {
     ssr: false,
@@ -53,6 +53,7 @@ export default function BlogIndexPage() {
 
     return (
         <>
+        <Navigator/>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-start z-30 bg-stone-200 bg-opacity-10 w-[90%] h-2/3 group hover:bg-opacity-50 rounded-lg p-6 overflow-y-auto">
                 <h1 className="font-badeen text-5xl text-left mb-6">Blog</h1>
 
