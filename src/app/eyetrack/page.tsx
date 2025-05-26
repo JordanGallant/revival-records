3f783e8a21f3aebba369645b0d63705c3b15de94
Jordan <39-12602@sae.edu.za>
2025-05-26 19:25:05 +0200

    fix eyetrack

--- src/app/eyetrack/page.tsx
+++ src/app/eyetrack/page.tsx
@@ -5,7 +5,7 @@ const EyeTrack: React.FC = () => {
     return (
         <>
             <div className="h-screen w-screen bg-black">
-                <iframe src="https://monumental-nasturtium-b6223b.netlify.app/"
+                <iframe src="https://eye-track-orcin.vercel.app/"
                     style={{ width: '100%', height: '100%', border: 'none' }}
                 >
                 </iframe>
